e4bc11ff9de0404cd199c476d3eb4b198aec60a8
kgrad <k.gradek2001@gmail.com>
2024-11-09 03:40:27 +0100

    aa

--- robot_slam/launch/rplidar.launch.py
+++ robot_slam/launch/rplidar.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0',
                 'serial_baudrate': 115200,  # A1 / A2
                 # 'serial_baudrate': 256000, # A3
                 'frame_id': 'laser',
